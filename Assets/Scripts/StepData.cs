053ce70e90c9095f3b61c4c085a47b552701b804
Antje Lemberg <frontantje@gmail.com>
2025-11-29 18:12:10 +0100

    feat: scaffold basic step editor setup

---
+++ Assets/Scripts/StepData.cs
@@ -0,0 +1,13 @@
+public class StepData
+{
+    public string ActionName { get; set; }
+    public float Value { get; set; }
+
+    public StepData(string name, float val)
+    {
+        ActionName = name;
+        Value = val;
+    }
+
+    public override string ToString() => $"{ActionName}: {Value}";
+}
